b05f40098d2c401e30c2a195583bad29ecbec7e4
DataJoshua <rodriguez.taco66@gmail.com>
2022-09-21 16:22:51 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+
+    <h1>hola mundo</h1>
+    <h3>mi nomre es joshua</h3>
+    </>
+  )
+}
+
+export default App;
